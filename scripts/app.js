33ba6a0521353ccd8f3481aea7244fee57d1f09b
riisitera <rantonov@tktk.ee>
2025-05-15 20:18:42 +0300

    carousel

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
+const slides = document.getElementsByClassName('carousel__item');
+const slidesTotal = slides.length;
+const nextButton = document.querySelector('#carousel__button--next');
+const prevButton = document.querySelector('#carousel__button--prev');
+
+let slidePosition = 0;
 let moviePosterContainer = document.querySelector('.movie-poster');
 let modal = document.querySelector('#myModal');
 let closeBtn = document.querySelector('.closeBtn');
@@ -16,4 +22,34 @@ modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
+});
+
+nextButton.addEventListener('click', () => {
+    for(let slide of slides) {
+        slide.classList.remove('carousel__item--visible');
+    }
+
+    if(slidePosition === slidesTotal - 1) {
+        slidePosition = 0;
+    }
+    else {
+        slidePosition++;
+    }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
+});
+
+prevButton.addEventListener('click', () => {
+    for(let slide of slides) {
+        slide.classList.remove('carousel__item--visible');
+    }
+
+    if(slidePosition === 0) {
+        slidePosition = slidesTotal - 1;
+    }
+    else {
+        slidePosition--;
+    }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
 });
